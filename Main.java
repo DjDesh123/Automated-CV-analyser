5ea84b1f900db60d122bda77748c2fa8497ec10b
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-17 14:45:07 +0000

    took out the main menu stuff in main its a lot cleaner and makes more sense being in a different file entirely

--- Main.java
+++ Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 // ideally tidy up the log in page and instead of trying to store an invalid option we will stop it there with a certain command
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         //loaded my database in first
+        Scanner sc = new Scanner(System.in);
         LogInDatabase db = new LogInDatabase();
         MenuSchedular ms = new MenuSchedular(sc);
 
         // Check if database file exists
+        //this should be in the databse file because its taking space
         File DatabaseFile = new File("SavedDatabase.dat");
         if (DatabaseFile.exists()) {
             System.out.println("Database file exists at: " + DatabaseFile.getAbsolutePath());
@@ -20,39 +21,21 @@ public class Main {
 
 
 
-        int UserChoice = 0;
+        ms.DisplayMenu();
 
 
-        do {
-
-            ms.DisplayMenu();
-            UserChoice=ms.getUserChoice();
-
-
-            // this is a switch case for either entering the logIn Page or the SignUP Page or if a miustake is made to clear screen and loop back
-            switch (UserChoice) {
-                case 1:
-                    LogInPage.LogIn(sc,db );
-                    break;
-                case 2:
-                    SignUpPage.SignUp(sc,db);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Try again");
-
-                    ClearScreen.clearScreen();
-                    break;
-            }
-
-        }while( UserChoice < 1 || UserChoice > 2);
     }
 
 
 }
 
 //make the data from the hashtable to read and also export in a file so it won't just erase each time.
-//make add job requirments an array because thats a lot better to deal with and make a loop so i can loop how many requirements they want to add
+//make add job requirements an array because thats a lot better to deal with and make a loop so i can loop how many requirements they want to add
 // could write a for loop by just asking the user how many entries they want to add
+// add a third option to end the program
+//make an int validation checker so it will constantly check if the user entered a correct integer then the do while will check if its in range
+// the file handling should be in the LogInDatabse not in main its better to encapsulate here
+//save the database before ending
 
 
 /*
