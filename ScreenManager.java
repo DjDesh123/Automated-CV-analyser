c9e11e4c6dcd97052f02c57f0d99cea788396070
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-18 15:52:54 +0000

    renamed the class just thought its a lot more concise

---
+++ ScreenManager.java
@@ -0,0 +1,31 @@
+// https://www.quora.com/How-do-I-clear-the-console-screen-in-Java is where i got this code do want to make it more origrnal tho
+public class ScreenManager {
+
+    private static String Os;
+
+    // a constructer to test the os runtime.
+    public ScreenManager() {
+        this.Os = System.getProperty("os.name").toLowerCase();
+    }
+
+
+    // this will actually clear the screen
+    public static void ClearScreen(){
+        try{
+            if(Os.contains("win")){
+                // how this works is by telling the windows interpeter "cmd" to run cls and then end
+                // we use inheritIO to redirect any input or output from the command prompt to the java program whislt we use .start to start the command prompt with the cls command and waitfor() which waits for the external process to happen before processing
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else{
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.err.println("Error clearing screen: " + e.getMessage());  // Handle any exceptions (e.g., IO or InterruptedException)
+
+        }
+
+    }
+}
+
+
