2fddfba14f72347d1e2d0e3075eae58bf9e4f6c7
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-18 18:16:18 +0000

    got rid of the username parameter

--- DashboardRedirector.java
+++ DashboardRedirector.java
@@ -1,11 +1,11 @@
 // What i want this class to do is basically take from my database and then go "oh they say they are an applicant/recuiter then sent to their repsect dashbaords "
 public class DashboardRedirector {
 
-    public static void Redrict(String AccountType ,String Username) {
+    public static void Redrict(String AccountType ) {
 
         if (AccountType.equals("Recruiter")) {
             //sends him to the RecruiterDashboard.
-            RecruiterDashboard.ShowRecruiterDashboard(Username);
+            RecruiterDashboard.ShowRecruiterDashboard();
         }
         else if (AccountType.equals("Applicant")) {
             ApplicantDashboard.ShowApplicantDashboard();
