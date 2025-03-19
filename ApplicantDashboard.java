c22bf6c569ca11bc3cef3b581b86fb9a94d4fbb8
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-19 23:03:23 +0000

    added the plan and going to go back and implement it all tommorow my own way

--- ApplicantDashboard.java
+++ ApplicantDashboard.java
@@ -1,6 +1,34 @@
+import java.rmi.activation.ActivationGroup_Stub;
+import java.util.Scanner;
+
 public class ApplicantDashboard {
     public static void ShowApplicantDashboard(){
         System.out.println("Applicant Dashboard");
+        Scanner sc = new Scanner(System.in);
+        JobDatabase jdb = new JobDatabase();
+        MenuSchedular ms = new MenuSchedular(sc);
+
+
+        if(!jdb.ShowAllJobs()){
+            System.out.println("redirecting to main menu .....");
+            ms.DisplayMenu();
+        }
+
+
     }
 
+
+
+
+
 }
+    /* for this section be able to
+     - see all available job postings
+     - allow the user to post their cv
+     - for it to be analysed by the nlp
+     - the nlp to use the requirements as the searching tool
+     - a saving mechanism for jobs they applied for
+     - to send it back to the applicant to be able to see who wants the job
+     - and to show which requirement the user hit
+     */
+
