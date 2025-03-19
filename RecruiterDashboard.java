de390277a8e5b805825f659013ac4d5cd813de15
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-19 21:03:52 +0000

    change the layout of the code made the menu display a lot mreo clearer and understandable . added a while true as well to loop when the user inputs something wrong. section finished removed working on comments

--- RecruiterDashboard.java
+++ RecruiterDashboard.java
@@ -9,57 +9,54 @@ public class RecruiterDashboard {
 
 
     public static void ShowRecruiterDashboard() {
+
         // greets the user
-        System.out.print("Welcome to the Recruiter Dashboard");
+        System.out.println("Welcome to the Recruiter Dashboard");
         JobDatabase jd = new JobDatabase();
         LogInDatabase ld = new LogInDatabase();
         Scanner sc = new Scanner(System.in);
         MenuSchedular ms = new MenuSchedular(sc);
 
+        while (true) {
+            ScreenManager.ClearScreen();
+            // to check if the user has the any jobs saved in their database
+            if (!jd.ShowAllJobs()) {
+                System.out.println("redirecting back to Add job menu .....");
+                jd.AddJob(sc);
+            }
 
 
-        // to check if the user has the any jobs saved in their database
-        if (!jd.ShowAllJobs()) {
-            System.out.println("No jobs found");
-            System.out.println("redirecting back to Add job menu .....");
-            jd.AddJob();
-        }
+            System.out.println("\nDo you wish to:");
+            System.out.println("1. Add a job");
+            System.out.println("2. Edit a job");
+            System.out.println("3. Delete a job");
+            System.out.println("4. Exit to main menu");
 
-        int DashboardChoice = IntValidation.ValidateInt("Do you wish to add, edit or delete job posting (1-4)");
+            int DashboardChoice = IntValidation.ValidateInt("Enter your choice (1-4)", sc);
 
-        // chose a switch statement instead of an if statement to make it neater and as well just want to tidy this code up with some cosntants for easier reading
-        //I want to add a loop to this to make it look a lot better and keeps the flow of the project
-        switch (DashboardChoice) {
-            case ADD_JOB:
-                jd.AddJob();
-                break;
-            case EDIT_JOB:
-                jd.EditJob();
-                break;
-            case DELETE_JOB:
-                jd.DeleteJob();
-                break;
-            case EXIT:
-                System.out.println("redriving back to main menu .....");
-                ms.DisplayMenu();
-                break;
-            default:
-                System.out.println("Invalid choice");
 
+            // chose a switch statement instead of an if statement to make it neater and as well just want to tidy this code up with some cosntants for easier reading
+            //I want to add a loop to this to make it look a lot better and keeps the flow of the project
+            switch (DashboardChoice) {
+                case ADD_JOB:
+                    jd.AddJob(sc);
+                    break;
+                case EDIT_JOB:
+                    jd.EditJob(sc);
+                    break;
+                case DELETE_JOB:
+                    jd.DeleteJob(sc);
+                    break;
+                case EXIT:
+                    System.out.println("redirecting back to main menu .....");
+                    ms.DisplayMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+
+            }
         }
     }
 
+
 }
-
-
-
-
-
-
-
-// need an if statement to see if the user has added any job posting previously
-//need to add a new storage for that as i dont want it to be cluttered and i can have it link to the other database
-// then have a list of the roles but i also need this database not to wipe each time i create something so need ot find a way of dealing with that
-// but i can make it read data that we put in a file in the first place like my previosu project
-// deciding to use dat files because it stores in binary can be encrypted and als0 preserve object state and is also layed out like a database
-
