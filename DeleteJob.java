4ae7324f7ba9d57daa3ad25b220d53dbe9f7960f
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-19 17:49:14 +0000

    i seen that making lots of uneeded statics isnt very useful and in my database it should also be able to handle the changes that comes with databases so i removed the statics and the seperate classes and put it all under one class and just call the instances instead. i think it is a better structured way of dealing with situations like this and ups the quality massively

--- DeleteJob.java
+++
@@ -1,5 +0,0 @@
-public class DeleteJob {
-    public static void DeletingJob() {
-        System.out.println("Deleting Job...");
-    }
-}
--- EditJob.java
+++
@@ -1,5 +0,0 @@
-public class EditJob {
-    public static void EditingJob() {
-        System.out.println("Editing Job...");
-    }
-}
