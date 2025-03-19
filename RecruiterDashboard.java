dd2d2c1df05a05420c4a78fea4c6053f1fdb41c6
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-19 17:51:47 +0000

    added some constants for a cleaner menu and also decided on doing instances instead of doing static because it seems better and makes more logical sense

--- RecruiterDashboard.java
+++ RecruiterDashboard.java
@@ -1,35 +1,54 @@
 import java.util.Scanner;
 
 public class RecruiterDashboard {
+    // Constants for cleaner menu options
+    private static final int ADD_JOB = 1;
+    private static final int EDIT_JOB = 2;
+    private static final int DELETE_JOB = 3;
+    private static final int EXIT = 4;
+
+
     public static void ShowRecruiterDashboard() {
         // greets the user
         System.out.print("Welcome to the Recruiter Dashboard");
         JobDatabase jd = new JobDatabase();
         LogInDatabase ld = new LogInDatabase();
         Scanner sc = new Scanner(System.in);
+        MenuSchedular ms = new MenuSchedular(sc);
+
 
 
         // to check if the user has the any jobs saved in their database
         if (!jd.ShowAllJobs()) {
             System.out.println("No jobs found");
             System.out.println("redirecting back to Add job menu .....");
-            AddJob.AddingJob();
+            jd.AddJob();
         }
 
-        int DashboardChoice = IntValidation.ValidateInt("Do you wish to add, edit or delete job posting (1-3)");
+        int DashboardChoice = IntValidation.ValidateInt("Do you wish to add, edit or delete job posting (1-4)");
 
         // chose a switch statement instead of an if statement to make it neater and as well just want to tidy this code up with some cosntants for easier reading
+        //I want to add a loop to this to make it look a lot better and keeps the flow of the project
         switch (DashboardChoice) {
-            case 1:
-                AddJob.AddingJob();
+            case ADD_JOB:
+                jd.AddJob();
                 break;
-            case 2:
-                DeleteJob.DeletingJob();
+            case EDIT_JOB:
+                jd.EditJob();
                 break;
-            case 3:
-                EditJob.EditingJob();
+            case DELETE_JOB:
+                jd.DeleteJob();
+                break;
+            case EXIT:
+                System.out.println("redriving back to main menu .....");
+                ms.DisplayMenu();
+                break;
+            default:
+                System.out.println("Invalid choice");
+
         }
     }
+
 }
 
 
