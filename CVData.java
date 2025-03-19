d9c2debc2227fd7b55f8104feaf392707f08e2f7
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-19 22:57:44 +0000

    just stores the raw data of the cv

---
+++ CVData.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+public class CVData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    public String Username;
+    public String CVContent;
+
+
+    public CVData(String Username, String Content) {
+        this.Username = Username;
+        this.CVContent = CVContent;
+    }
+
+    // getter
+
+    public String GetUsername(){
+        return Username;
+    }
+
+    public String GetCVContent(){
+        return CVContent;
+    }
+
+}
