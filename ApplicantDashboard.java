679028fbc72a7a8088e2810c922b70dcf509bbf8
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-20 23:09:44 +0000

    added conts to add a level of clean and presentation

--- ApplicantDashboard.java
+++ ApplicantDashboard.java
@@ -2,6 +2,11 @@ import java.rmi.activation.ActivationGroup_Stub;
 import java.util.Scanner;
 
 public class ApplicantDashboard {
+    private static final int UPLOAD_CV =  1;
+    private static final int MORE_DETAILS =  2;
+    private static final int DISPLAYS_MENU =  3;
+
+
     public static void ShowApplicantDashboard(String Username){
         System.out.println("Applicant Dashboard");
         Scanner sc = new Scanner(System.in);
@@ -9,24 +14,26 @@ public class ApplicantDashboard {
         MenuSchedular ms = new MenuSchedular(sc);
         CVDatabase cvdb = new CVDatabase();
 
-        // shows all jobs and if the theres no jobs present then it will send them back to the main menu
+
+
+        // shows all jobs and if the there's no jobs present then it will send them back to the main menu
         if(!jdb.ShowAllJobs()){
             System.out.println("redirecting to main menu .....");
             ms.DisplayMenu();
         }
 
     while (true) {
-
+        // allows the user to navigate the options
         int MenuChoice = IntValidation.ValidateInt("Do you wish to post your cv to a job, see more details or Exit to main menu (1-3)", sc);
 
         switch (MenuChoice) {
-            case 1:
+            case UPLOAD_CV:
                 cvdb.UploadCV(Username, sc);
                 break;
-            case 2:
+            case MORE_DETAILS:
                 jdb.MoreDetails(sc);
                 break;
-            case 3:
+            case DISPLAYS_MENU:
                 ms.DisplayMenu();
                 break;
             default:
@@ -45,12 +52,6 @@ public class ApplicantDashboard {
 
 }
     /* for this section be able to
-     - see all available job postings
-     - allow the user to post their cv
-     - for it to be analysed by the nlp
-     - the nlp to use the requirements as the searching tool
-     - a saving mechanism for jobs they applied for
      - to send it back to the applicant to be able to see who wants the job
-     - and to show which requirement the user hit
      */
 
