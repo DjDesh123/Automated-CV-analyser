b49ec746e1d6f331bd0d56b4f2cd512ce1acbd5e
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-20 19:25:02 +0000

    added user to get the infomation that i needed because realistically i only needed the logged in user not certain users themselves at this current point

--- DashboardRedirector.java
+++ DashboardRedirector.java
@@ -1,14 +1,14 @@
 // What i want this class to do is basically take from my database and then go "oh they say they are an applicant/recuiter then sent to their repsect dashbaords "
 public class DashboardRedirector {
 
-    public static void Redrict(String AccountType ) {
+    public static void Redrict(String AccountType,String Username ) {
 
         if (AccountType.equals("Recruiter")) {
             //sends him to the RecruiterDashboard.
-            RecruiterDashboard.ShowRecruiterDashboard();
+            RecruiterDashboard.ShowRecruiterDashboard(Username);
         }
         else if (AccountType.equals("Applicant")) {
-            ApplicantDashboard.ShowApplicantDashboard();
+            ApplicantDashboard.ShowApplicantDashboard(Username);
         }
         else{
             System.out.println("Invalid Account Type");
