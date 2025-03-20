e67ceaa2aff35be3186cda9effcfbede7562668e
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-20 19:20:32 +0000

    i added a menu to allow the user to have access to the other instances

--- ApplicantDashboard.java
+++ ApplicantDashboard.java
@@ -2,17 +2,39 @@ import java.rmi.activation.ActivationGroup_Stub;
 import java.util.Scanner;
 
 public class ApplicantDashboard {
-    public static void ShowApplicantDashboard(){
+    public static void ShowApplicantDashboard(String Username){
         System.out.println("Applicant Dashboard");
         Scanner sc = new Scanner(System.in);
         JobDatabase jdb = new JobDatabase();
         MenuSchedular ms = new MenuSchedular(sc);
+        CVDatabase cvdb = new CVDatabase();
 
-
+        // shows all jobs and if the theres no jobs present then it will send them back to the main menu
         if(!jdb.ShowAllJobs()){
             System.out.println("redirecting to main menu .....");
             ms.DisplayMenu();
         }
+
+    while (true) {
+
+        int MenuChoice = IntValidation.ValidateInt("Do you wish to post your cv to a job, see more details or Exit to main menu (1-3)", sc);
+
+        switch (MenuChoice) {
+            case 1:
+                cvdb.UploadCV(Username, sc);
+                break;
+            case 2:
+                jdb.MoreDetails(sc);
+                break;
+            case 3:
+                ms.DisplayMenu();
+                break;
+            default:
+                System.out.println("Invalid choice");
+
+        }
+    }
+
 
 
     }
