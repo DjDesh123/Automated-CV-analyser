146d4868459a565236c27471373669be0c0f393a
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-21 23:52:08 +0000

    added some more to the CVData object that now allowes me tyo get the jobName and the MatchedRequirements

--- CVData.java
+++ CVData.java
@@ -1,19 +1,27 @@
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
 public class CVData implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
     public String Username;
     public String CVContent;
+    public String JobName;
+    public List<String> MatchedRequirements;
 
 
-    public CVData(String Username, String CVContent) {
+    public CVData(String Username, String CVContent, String JobName, List<String> matched) {
         this.Username = Username;
         this.CVContent = CVContent;
+        this.JobName = JobName;
+        this.MatchedRequirements = MatchedRequirements;
     }
 
     // getter
+
+    public String GetJobName() {return JobName;}
 
     public String GetUsername(){
         return Username;
@@ -22,5 +30,8 @@ public class CVData implements Serializable {
     public String GetCVContent(){
         return CVContent;
     }
+
+    public List<String> GetMatchedRequirements() {return MatchedRequirements;}
+
 
 }
