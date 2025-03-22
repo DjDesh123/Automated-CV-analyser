d3d0c437918dc0354eba47e00715cfa8f80dda41
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-22 14:50:58 +0000

    rearrange the code and added a do while instead of a while true as its bad practice and was just a temp placeholder till i got the method actually working

--- RecruiterDashboard.java
+++ RecruiterDashboard.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class RecruiterDashboard {
+
     // Constants for cleaner menu options
     private static final int ADD_JOB = 1;
     private static final int EDIT_JOB = 2;
@@ -8,54 +9,67 @@ public class RecruiterDashboard {
     private static final int EXIT = 4;
 
 
-    public static void ShowRecruiterDashboard(String Username) {
+    public static void ShowRecruiterDashboard(String Username,Scanner sc ) {
+
+        JobDatabase jd = new JobDatabase();
+        MenuSchedular ms = new MenuSchedular(sc);
+        NotificationSystem ns = new NotificationSystem();
+
+
+        int DashboardChoice;
+
+        // removes all the loading database messages
+        ScreenManager.ClearScreen();
 
         // greets the user
-        System.out.println("Welcome to the Recruiter Dashboard");
-        JobDatabase jd = new JobDatabase();
-        LogInDatabase ld = new LogInDatabase();
-        Scanner sc = new Scanner(System.in);
-        MenuSchedular ms = new MenuSchedular(sc);
+        System.out.println("\n\nWelcome to the Recruiter Dashboard \n\n");
 
-        while (true) {
-            ScreenManager.ClearScreen();
-            // to check if the user has the any jobs saved in their database
-            if (!jd.ShowUserJobs(Username)) {
-                System.out.println("redirecting back to Add job menu .....");
-                jd.AddJob(sc,Username);
-            }
+        //displays all the notifications
+        ns.ShowNotification(Username);
 
+        // to check if the user has the any jobs saved in their database
+        if (!jd.ShowUserJobs(Username)) {
+            System.out.println("redirecting back to Add job menu .....");
+            jd.AddJob(sc,Username);
+        }
 
+        do{
+
+            //Displays the menu
             System.out.println("\nDo you wish to:");
             System.out.println("1. Add a job");
             System.out.println("2. Edit a job");
             System.out.println("3. Delete a job");
             System.out.println("4. Exit to main menu");
 
-            int DashboardChoice = IntValidation.ValidateInt("Enter your choice (1-4)", sc);
-
+            DashboardChoice = IntValidation.ValidateInt("Enter your choice (1-4)", sc);
 
             // chose a switch statement instead of an if statement to make it neater and as well just want to tidy this code up with some cosntants for easier reading
             //I want to add a loop to this to make it look a lot better and keeps the flow of the project
             switch (DashboardChoice) {
+
                 case ADD_JOB:
                     jd.AddJob(sc,Username);
                     break;
+
                 case EDIT_JOB:
                     jd.EditJob(sc);
                     break;
+
                 case DELETE_JOB:
                     jd.DeleteJob(sc);
                     break;
+
                 case EXIT:
-                    System.out.println("redirecting back to main menu .....");
+                    System.out.println("Redirecting to Main Menu....");
                     ms.DisplayMenu();
                     break;
+
                 default:
-                    System.out.println("Invalid choice");
+                    System.out.println("Invalid choice! Must be an integer from 1 to 4");
 
             }
-        }
+        }while(DashboardChoice < ADD_JOB || DashboardChoice > EXIT);
     }
 
 
