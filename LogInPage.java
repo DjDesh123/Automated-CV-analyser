ffe516b4743e64adedadea63694ee098e5b24a2e
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-22 14:46:47 +0000

    added some comments and split the code up to make it less cluttered. Removed uneeded debugging messages that was just cluttering the terminal.

--- LogInPage.java
+++ LogInPage.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class LogInPage {
     public static void LogIn(Scanner sc, LogInDatabase db) {
+        // clears the screen to allow a better user experience
         ScreenManager.ClearScreen();
+
+        String Username;
         boolean LogInSuccess = false;
         sc.nextLine();
 
         // if the file just been made and is empty then it will send you to the sign up page
-
         if(db.GetUserCredentialMap().isEmpty()){
             System.out.println("There are no user saved in the database.\n Redirecting to Sign up page....");
             SignUpPage.SignUp(sc, db);
@@ -21,8 +23,9 @@ public class LogInPage {
             System.out.println("WARNING: Passwords and username are cap sensitive");
 
             //asks for the username
-            String Username = StringValidation.ValidateString("Please enter your username:",sc);
+            Username = StringValidation.ValidateString("Please enter your username:",sc);
 
+            // checks if the username exists or not
             if (!db.GetUserCredentialMap().containsKey(Username)){
                 System.out.println("Username not found");
                 System.out.println("Try again or try a different username: ");
@@ -34,19 +37,16 @@ public class LogInPage {
             String Password = StringValidation.ValidateString("Please enter your password:",sc);
 
 
-            //compare with the linked list database to see if present
-            LogInSuccess = db.CheckUserCredentials(Username,Password);
+            //uses the username to check if the stored password and the entered passwords are the same
+            LogInSuccess = db.CheckPasswordIsValid(Username,Password);
 
 
-            if (LogInSuccess) {
-                UserInfo User = db.GetUser(Username);
-                String StoredAccountType = User.GetAccountType();
+          // keeps asking till LogInSuccess is true
+        } while (!LogInSuccess);
 
-                DashboardRedirector.Redrict(StoredAccountType,Username);
-            } else {
-                System.out.println("Invalid username or password");
-            }
-        } while (!LogInSuccess);// keeps asking until  its true
+        // goes to the dashboard redirector to send thme to the correct page
+        DashboardRedirector.Redrict(Username,sc,db);
+
     }
 }
 
