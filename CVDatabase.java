3c0b692b52d7c67d29d0c4f0a29cc4036d29b776
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-22 14:38:43 +0000

    edgecases to make sure the program doesnt just fail if something was inputted incorectly

--- CVDatabase.java
+++ CVDatabase.java
@@ -1,10 +1,12 @@
+import javax.print.attribute.standard.JobName;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class CVDatabase {
     //initalises the linkedhashmap
-    private final LinkedHashMap<String, CVData> CVHashMap;
+    final LinkedHashMap<String, CVData> CVHashMap;
 
     // Load CV database from file (if exists)
     public CVDatabase() {
@@ -20,12 +22,28 @@ public class CVDatabase {
     // this will be replaced as we are going to get the actual files and then rip it into this code via a folder or something
     public void UploadCV(String Username, Scanner sc) {
         NLPProcessor nlp = new NLPProcessor();
-        JobDatabase  jdb = new JobDatabase();
+        JobDatabase jdb = new JobDatabase();
+        CVDatabase cvdb = new CVDatabase();
 
-        jdb.ShowAllJobs();
+        boolean FoundJob;
+        String JobName;
+
+        // loops till its correct
+        do {
+            // shows all the avaliable jobs
+            jdb.ShowAllJobs();
+
+            // gets the job name
+            JobName = StringValidation.ValidateString("Enter the name of the job you wish to submit your cv to:", sc);
+
+            FoundJob = jdb.FindJob(JobName);
+        } while (!FoundJob);
+
+        System.out.println("Exited the loop. Moving forward...");
 
         // this was used to asks user to select a job then i call the GetRequirements to get the requirements
-        List<String> Requirements = jdb.GetRequirementsFromJob(sc);
+        List<String> Requirements = jdb.GetRequirementsFromJob(JobName, sc);
+
 
         System.out.println("Please enter/paste your CV (press Enter twice to finish):");
 
@@ -48,18 +66,24 @@ public class CVDatabase {
         }
 
         // fianlly converts the StringBuilder back into a string then calls AnalyseAndMatch to show the the nlp to compare and show the matched requirements
-        List<String> Matched = nlp.AnalyzeAndMatch(String.valueOf(CVContent),Requirements);
+        List<String> Matched = nlp.AnalyzeAndMatch(String.valueOf(CVContent), Requirements);
+
         //displays matched requirements
         System.out.println("Matched Requirements: " + Matched);
 
         // Store the CV
-        CVData NewCV = new CVData(Username, CVContent.toString().trim());
-        //puts it in the cvhahap
+        CVData NewCV = new CVData(Username, CVContent.toString().trim(),JobName,Matched);
+
+        //puts it in the cvhashmap
         CVHashMap.put(Username, NewCV);
         System.out.println("CV uploaded successfully!");
 
         //saves this in the hashmap
         SaveDatabase();
+
+
+        //now sends a notifcation to the recruiter when the database is checked later on
+        jdb.CheckJobApplication(JobName,cvdb);
     }
 
     // Display a user's CV (for debugging/testing)
