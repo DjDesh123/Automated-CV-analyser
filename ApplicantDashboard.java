24effed180bc7da36ae08d08439e1cc317f03085
Deshawn Johal <deshawnjohal@gmail.com>
2025-03-23 17:34:15 +0000

    removed the while true and made the ui look a lot more presentable

--- ApplicantDashboard.java
+++ ApplicantDashboard.java
@@ -1,57 +1,49 @@
-import java.rmi.activation.ActivationGroup_Stub;
 import java.util.Scanner;
 
 public class ApplicantDashboard {
     private static final int UPLOAD_CV =  1;
     private static final int MORE_DETAILS =  2;
-    private static final int DISPLAYS_MENU =  3;
+    private static final int MAIN_MENU =  3;
 
 
-    public static void ShowApplicantDashboard(String Username){
+    public static void ShowApplicantDashboard(String Username, Scanner sc) {
         System.out.println("Applicant Dashboard");
-        Scanner sc = new Scanner(System.in);
         JobDatabase jdb = new JobDatabase();
         MenuSchedular ms = new MenuSchedular(sc);
         CVDatabase cvdb = new CVDatabase();
 
 
-
         // shows all jobs and if the there's no jobs present then it will send them back to the main menu
-        if(!jdb.ShowAllJobs()){
+        if (!jdb.ShowAllJobs()) {
             System.out.println("redirecting to main menu .....");
             ms.DisplayMenu();
         }
 
-    while (true) {
-        // allows the user to navigate the options
-        int MenuChoice = IntValidation.ValidateInt("Do you wish to post your cv to a job, see more details or Exit to main menu (1-3)", sc);
+        int MenuChoice;
 
-        switch (MenuChoice) {
-            case UPLOAD_CV:
-                cvdb.UploadCV(Username, sc);
-                break;
-            case MORE_DETAILS:
-                jdb.MoreDetails(sc);
-                break;
-            case DISPLAYS_MENU:
-                ms.DisplayMenu();
-                break;
-            default:
-                System.out.println("Invalid choice");
+        do {
+            System.out.println("1. Post your cv");
+            System.out.println("2. More details");
+            System.out.println("3. Exit");
+            // allows the user to navigate the options
+            MenuChoice = IntValidation.ValidateInt("Enter your choice (1-3)", sc);
 
-        }
+            switch (MenuChoice) {
+                case UPLOAD_CV:
+                    cvdb.UploadCV(Username, sc);
+                    break;
+                case MORE_DETAILS:
+                    jdb.MoreDetails(Username,sc);
+                    break;
+                case MAIN_MENU:
+                    ms.DisplayMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+
+            }
+        } while (MenuChoice < UPLOAD_CV || MenuChoice > MAIN_MENU);
     }
-
-
-
-    }
-
-
-
-
-
 }
-    /* for this section be able to
-     - to send it back to the applicant to be able to see who wants the job
-     */
+
 
